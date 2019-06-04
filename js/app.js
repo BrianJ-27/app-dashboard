7b1614b9c53ffb263fea9e28fa4b63392987962b
Brian <johnson8727@hotmail.com>
2019-06-03 23:23:36 -0400

    working on my chart.js graphs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 var ctx = document.getElementById('traffic-chart').getContext('2d');
-var myLineChart = new Chart(ctx, {
+var bar = document.getElementById('daily-chart').getContext('2d');
+
+var chart = new Chart(ctx, {
     type: 'line',
-    data: data,
+    data: {
+        labels: ['16-22', '23-29', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgba(8, 154, 245, 0.6)',
+            borderColor: '#B38F00',
+            data: [500, 1000, 1500, 2000, 2500]
+        }],
+       
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var myBarChart = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    },
     options: options
 });
