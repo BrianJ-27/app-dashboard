ce26b1e58d2748a4a3577ccd21b52243575206a3
Brian <johnson8727@hotmail.com>
2019-06-06 13:40:11 -0400

    Removed the legends from the top of charts  app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 var ctx = document.getElementById('traffic-chart').getContext('2d');
-
+Chart.defaults.global.legend.display = false
 
 var chart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ['16-22', '23-29', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
         datasets: [{
-            label: 'My First dataset',
             backgroundColor: 'rgba(8, 154, 245, 0.6)',
+            lineTension: 0.1,
             borderColor: '#B38F00',
-            data: [500, 1000, 1500, 2000, 2500]
-        }],
-       
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDassOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBackgroundColor: 'black',
+            pointBorderColor: 'white',
+            pointBorderWidth: 5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'green',
+            pointHoverBorderColor: 'orange',
+            pointHoverBorderWidth: 'grey',
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [500, 800, 450, 950, 675, 1100, 770, 1250, 650, 1500]
+        }
+     ]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        scales:{
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+
+    }
 });
 
 var bar = document.getElementById('daily-chart').getContext('2d');
@@ -24,7 +46,7 @@ var myBarChart = new Chart(bar, {
     data: {
         labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
         datasets:[{
-            data:[50, 100, 150, 200, 250],
+            data:[50, 100, 150, 75, 180, 100, 75],
             backgroundColor: 'rgba(8, 154, 245, 0.6)'
         }]
     },
