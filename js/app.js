3ff314d8015dcd88828093f9e60cfc06831b3513
Brian <johnson8727@hotmail.com>
2019-06-08 21:32:26 -0400

    worked on the message confirmation & error message via JS

--- js/app.js
+++ js/app.js
@@ -1,79 +1,5 @@
-var ctx = document.getElementById('traffic-chart').getContext('2d');
-Chart.defaults.global.legend.display = false
 
-var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['16-22', '23-29', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-        datasets: [{
-            backgroundColor: 'rgba(8, 154, 245, 0.6)',
-            lineTension: 0.1,
-            borderColor: '#B38F00',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDassOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBackgroundColor: 'black',
-            pointBorderColor: 'white',
-            pointBorderWidth: 5,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'green',
-            pointHoverBorderColor: 'orange',
-            pointHoverBorderWidth: 'grey',
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [500, 800, 450, 950, 675, 1100, 770, 1250, 650, 1500]
-        }
-     ]
-    },
-
-    // Configuration options go here
-    options: {
-        scales:{
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-
-    }
-});
-
-var bar = document.getElementById('daily-chart').getContext('2d');
-var myBarChart = new Chart(bar, {
-    type: 'bar',
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        datasets:[{
-            data:[50, 100, 150, 75, 180, 100, 75],
-            backgroundColor: 'rgba(8, 154, 245, 0.6)'
-        }]
-    },
-    options: {}
-});
-
-var doughnut = document.getElementById('doughnut-chart').getContext('2d');
-var myDoughnutChart = new Chart(doughnut, {
-    type: 'doughnut',
-    data: {
-        labels: ['phones', 'tablets', 'desktop'],
-        datasets:[{
-            data:[25, 50, 75],
-            backgroundColor: [
-                'green',
-                'teal',
-                'rgba(8, 154, 245, 0.6)' 
-           ]
-        }]
-    },
-    options: {
-        legend:{
-            display: true,
-        }
-    }
-});
-
+// Alert Box 
 const xAlert = document.querySelector(".xbox");
 
 xAlert.addEventListener("click", () => {
@@ -82,11 +8,33 @@ xAlert.addEventListener("click", () => {
 })
 
 //Form Controls
-const form = document.querySelector("form");
-const input = form.querySelector("#send , #messageField");
+const form = document.getElementById("form");
 
-form.addEventListener("submit", (e)=> {
-    preventDefault();
-    console.log(input.value);
-})
+
+const validate = () => {
+    let input = form.querySelector("#userField").value;
+    let msgField = form.querySelector("#messageField").value;
+        if(input == "" || msgField == ""){
+                let errorMessage = document.getElementById("errorMsg");
+                errorMessage.style.display = "flex"; 
+                errorMessage.style.justifyContent = "center";
+                errorMessage.style.color = "red";
+                errorMessage.innerHTML = "All Fields are Required!!" ; 
+            }else if(input !== "" && msgField !== ""){
+                let errorMessage = document.getElementById("errorMsg"); 
+                    errorMessage.style.display = "flex"; 
+                    errorMessage.style.alignItems = "center";
+                    errorMessage.style.justifyContent = "center";
+                    errorMessage.style.width= "100%"; 
+                    errorMessage.style.height= "70%"; 
+                    errorMessage.style.color = "Green";
+                    errorMessage.innerHTML = "Thank You Message Sent";
+                    form.style.display = "none"; 
+            }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+      validate();
+});
 
---
+++ js/chart.js
@@ -0,0 +1,76 @@
+var ctx = document.getElementById('traffic-chart').getContext('2d');
+Chart.defaults.global.legend.display = false
+
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['16-22', '23-29', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            backgroundColor: 'rgba(8, 154, 245, 0.6)',
+            lineTension: 0.1,
+            borderColor: '#B38F00',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDassOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBackgroundColor: 'black',
+            pointBorderColor: 'black',
+            pointBorderWidth: 5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'green',
+            pointHoverBorderColor: 'orange',
+            pointHoverBorderWidth: 'grey',
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [500, 800, 450, 950, 675, 1100, 770, 1250, 650, 1500]
+        }
+     ]
+    },
+
+    // Configuration options go here
+    options: {
+        scales:{
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+
+    }
+});
+
+var bar = document.getElementById('daily-chart').getContext('2d');
+var myBarChart = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        datasets:[{
+            data:[50, 100, 150, 75, 180, 100, 75],
+            backgroundColor: 'rgba(8, 154, 245, 0.6)'
+        }]
+    },
+    options: {}
+});
+
+var doughnut = document.getElementById('doughnut-chart').getContext('2d');
+var myDoughnutChart = new Chart(doughnut, {
+    type: 'doughnut',
+    data: {
+        labels: ['phones', 'tablets', 'desktop'],
+        datasets:[{
+            data:[25, 50, 75],
+            backgroundColor: [
+                'green',
+                'teal',
+                'rgba(8, 154, 245, 0.6)' 
+           ]
+        }]
+    },
+    options: {
+        legend: {
+            position: "right",
+            display: true
+          }
+    }
+});
