27fd1a616984409a543c80d48f6a7776c2abb81a
Brian Johnson <johnson8727@hotmail.com>
2019-06-09 20:08:33 -0400

    trying to fix notification box

--- js/app.js
+++ js/app.js
@@ -10,31 +10,54 @@ xAlert.addEventListener("click", () => {
 //Form Controls
 const form = document.getElementById("form");
 
-
 const validate = () => {
     let input = form.querySelector("#userField").value;
     let msgField = form.querySelector("#messageField").value;
         if(input == "" || msgField == ""){
+                input = "";
                 let errorMessage = document.getElementById("errorMsg");
                 errorMessage.style.display = "flex"; 
                 errorMessage.style.justifyContent = "center";
                 errorMessage.style.color = "red";
                 errorMessage.innerHTML = "All Fields are Required!!" ; 
-            }else if(input !== "" && msgField !== ""){
-                let errorMessage = document.getElementById("errorMsg"); 
+            }else if(input !== "" && msgField !== "") {
+                    let errorMessage = document.getElementById("errorMsg"); 
+                    errorMessage.innerHTML = "Your Message has been Sent";
                     errorMessage.style.display = "flex"; 
                     errorMessage.style.alignItems = "center";
                     errorMessage.style.justifyContent = "center";
                     errorMessage.style.width= "100%"; 
                     errorMessage.style.height= "70%"; 
                     errorMessage.style.color = "Green";
-                    errorMessage.innerHTML = "Thank You Message Sent";
-                    form.style.display = "none"; 
+                    form.style.display = "none";   
+                    let noform =  form.style.display = "none";       
             }
+        return noform;    
+}
+
+const resetForm = () =>{
+  if(noform){
+      errorMessage.style.display = "none";
+      form.style.display = "block";
+  }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-      validate();
+    validate();  
+    resetForm();
 });
+
+//Notification Message Box
+//
+  $(".bell-alert").click(function() {
+    $(".notify-message-box").slideDown(1000);
+  });
+
+  
+
+
+ 
+  
+
 
