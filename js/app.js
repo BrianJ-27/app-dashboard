fa725dfb7efba28f8e7e9c664b2cf1b81150c0ce
Brian Johnson <johnson8727@hotmail.com>
2020-02-18 18:53:26 -0500

    added ES6 Modules and Class Methodology to code

--- js/app.js
+++ js/app.js
@@ -1,92 +1,118 @@
-// const getEmailSetting = JSON.parse(localStorage.getItem('emailSave'));
-// const getProfileSetting = JSON.parse(localStorage.getItem('profileSave'));
-// document.getElementById("localSaveEmail").checked = getEmailSetting;
-// document.getElementById("localSaveProfile").checked = getProfileSetting;
+ export default class AppDashboard {
+  /**=-=-=-=-CLASS PROPERTIES/VARIABLES=-=-=-=- */
+  constructor(){
+    // Alert Box 
+    this.xAlert = document.querySelector(".xbox");
+    //Form Controls
+    this.form = document.getElementById("form");
+    this.notesClose = document.getElementById("message-container"); 
+    this.alertBell = document.querySelector(".bell-alert");
+    this.events();
+  }
 
-// Alert Box 
-const xAlert = document.querySelector(".xbox");
+/**=-=-=-=-CLASS EVENTS=-=-=-=- */
+  events(){
+    //Dispays notification message box when user clicks on bell
+    this.alertBell.addEventListener("click", this.displayMessage);
+   
+    this.xAlert.addEventListener("click", this.closeAlert);
+    //Once user clicks any of the alerts in the message container the green notify dot disappears 
+    this.notesClose.addEventListener("click", this.removeAlerts);
+     
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.validateForm();  
+  //Once the form is successfully sent, the form field will return again
+      setTimeout(function () {
+        let errorMessage = document.getElementById("errorMsg"); 
+        let input = form.querySelector("#userField");
+        input.value = "";
+        let msgField = form.querySelector("#messageField");
+        msgField.value = "";
+        errorMessage.style.display = "none";
+        form.style.display = "block"; 
+        }, 1500);
+  });
+  //LOCAL STORAGE EVENT
+  document.addEventListener('DOMContentLoaded', ()=>{
+    if(this.supportsLocalStorage()){
+      const saveButton = document.getElementById("setLocalSettings");
+      const cancelButton = document.getElementById("clearLocalSettings");
+      this.getSetingsStorage();
+    
+        saveButton.addEventListener("click", ()=> {
+          this.saveEmailSettings();
+          this.saveProfileSettings();
+          this.timeZoneSetting();
+        });
+    
+        cancelButton.addEventListener("click", ()=> {
+          this.clearSettings();
+        }); 
+      } 
 
-xAlert.addEventListener("click", () => {
-  let alertBox = document.querySelector(".alert-wrapper");
-  alertBox.style.display = "none";
-});
+  });
+  
+  }
+  /**=-=-=-=-CLASS METHODS/FUNCTIONS=-=-=-=- */
 
-//Form Controls
-const form = document.getElementById("form");
+//Removes green alert circle when first alert is removed 
+//Then removes all remaining alerts
+  removeAlerts(e){
+    let targetElement = e.target.parentElement;
+    let greenCircle = document.querySelector(".notification-circle");
+    if(targetElement.classList.contains("hideMe")){
+      targetElement.remove();
+      greenCircle.style.display ="none";
+    }
+  }
 
-const validate = () => {
+//closes opening long blue alert headline
+  closeAlert(){
+    let alertBox = document.querySelector(".alert-wrapper");
+    alertBox.style.display = "none";
+  }
+  
+//Dispays notification message box at the top of dashboard
+  displayMessage(){
+    let alertMsg = document.querySelector(".notify-message-box");
+    alertMsg.style.display ="flex";
+  }
+
+//checks user input and alerts users if with message
+  validateForm(){
     let input = form.querySelector("#userField").value;
     let msgField = form.querySelector("#messageField").value;
-        if(input == "" || msgField == ""){
-                input = "";
-                let errorMessage = document.getElementById("errorMsg");
-                errorMessage.style.display = "flex"; 
-                errorMessage.style.justifyContent = "center";
-                errorMessage.style.color = "red";
-                errorMessage.innerHTML = "All Fields are Required!!" ; 
-            }else if(input !== "" && msgField !== "") {
-                    let errorMessage = document.getElementById("errorMsg"); 
-                    errorMessage.innerHTML = "Your Message has been Sent";
-                    errorMessage.style.display = "flex"; 
-                    errorMessage.style.alignItems = "center";
-                    errorMessage.style.justifyContent = "center";
-                    errorMessage.style.width= "100%"; 
-                    errorMessage.style.height= "70%"; 
-                    errorMessage.style.color = "Green";
-                    form.style.display = "none";        
-            }  
-};
+      if(input == "" || msgField == ""){
+         input = "";
+         let errorMessage = document.getElementById("errorMsg");
+         errorMessage.classList.add("error-msg-1");
+         errorMessage.style.display = "flex";
+         errorMessage.innerHTML = "All Fields are Required!!" ; 
+      }else if(input !== "" && msgField !== "") {
+         let errorMessage = document.getElementById("errorMsg"); 
+         errorMessage.classList.add("error-msg-2");
+         errorMessage.style.display = "flex";
+         errorMessage.innerHTML = "Your Message has been Sent";
+         form.style.display = "none";        
+      } 
+  }
 
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    validate();  
-//Once the form is successfully sent, the form field will return again
-    setTimeout(function () {
-      let errorMessage = document.getElementById("errorMsg"); 
-      let input = form.querySelector("#userField").value = "";
-      let msgField = form.querySelector("#messageField").value = "";
-      errorMessage.style.display = "none";
-      form.style.display = "block"; 
-      }, 1500);
-});
-
-//Dispays notification message box using Jquery
-  $(".bell-alert").click(function() {
-    $(".notify-message-box").slideDown(900);
-  });
-
-//When user clicks this function, it will remove the li elements (alert notifications) from the screen
-  const closeNotes = (noteId) => { 
-    let note = document.getElementById(noteId); 
-    note.style.display = "none";
-  };
-
-//Once user clicks any of the alerts in the message container the green notify dot disappears
-  const notesClose = document.getElementById("message-container");
-  const greenCircle = document.querySelector(".notification-circle");
-  
-  notesClose.addEventListener("click", () =>{
-      greenCircle.style.display ="none";
-  });
-  
-//LOCAL STORAGE
-
-//setting up shell functions that will be called in the event listeners below
+// LOCAL STORAGE METHODS/FUNCTIONS
 
 //function that checks if any browser supports local storage
-const supportsLocalStorage = () => {
+ supportsLocalStorage (){
   try {
-  return 'localStorage' in window && window['localStorage'] !== null;
-  }
-  catch(e){
-    return false;
-  }
-};
+    return 'localStorage' in window && window['localStorage'] !== null;
+    }
+    catch(e){
+      return false;
+    }
+ }
 
-// function that first grabs the on/off toggle input for email setting and checks if user turns it on or off
-// this function will be called inside the saveButton event listener
-const saveEmailSettings = () => {
+//get on/off toggle input for email setting and checks if user turns it on or off
+//this function will be called inside the saveButton event listener
+saveEmailSettings (){
   let emailSave = document.getElementById("localSaveEmail");
   if(emailSave.checked){
     localStorage.setItem("emailSave", "true");
@@ -95,11 +121,11 @@ const saveEmailSettings = () => {
     localStorage.setItem("emailSave", "false");
     return false;
   }
-};
+ }
 
-// function that first grabs the on/off toggle input for the profile setting and checks if user turns it on or off
-// this function will be called inside the saveButton event listener
-const saveProfileSettings = () => {
+//gets on/off toggle input for the profile setting & checks if user turns it on or off
+//this function will be called inside the saveButton event listener
+ saveProfileSettings(){
   let profileSave = document.getElementById("localSaveProfile");
   if(profileSave.checked){
     localStorage.setItem("profileSave", "true");
@@ -108,53 +134,33 @@ const saveProfileSettings = () => {
     localStorage.setItem("profileSave", "false");
     return false;
   }
-};
+ }
 
-const timeZoneSetting = () =>{
+ timeZoneSetting(){
   let timeZoneSave = document.getElementById("localSaveTz");
-   localStorage.setItem('timeZoneSave', timeZoneSave.selectedIndex);
-};
+  localStorage.setItem('timeZoneSave', timeZoneSave.selectedIndex);
+ }
 
-const getSetingsStorage =() => {
+ getSetingsStorage(){
   const getEmailSetting = JSON.parse(localStorage.getItem('emailSave'));
   const getProfileSetting = JSON.parse(localStorage.getItem('profileSave'));
   const getTimeZoneSetting = JSON.parse(localStorage.getItem('timeZoneSave'));
   document.getElementById("localSaveEmail").checked = getEmailSetting;
   document.getElementById("localSaveProfile").checked = getProfileSetting;
   document.getElementById("localSaveTz").selectedIndex = getTimeZoneSetting;
-};
+ }
 
-
-
-// function that is called in when user clicks the cancel button & it removes saved settings
-const clearSettings = () => {
+ //user clicks the cancel button & it removes all saved settings
+ clearSettings(){
   document.getElementById("localSaveTz").selectedIndex = 0;
   localStorage.removeItem("emailSave");
   localStorage.removeItem("profileSave");
-  localStorage.removeItem("localSaveTz");
+  localStorage.removeItem("timeZoneSave");
   location.reload();
-};
+ }
+}
 
-window.onload =() =>{
-//check to see if we can use local storage once page loads
-  if(supportsLocalStorage()){
-  const saveButton = document.getElementById("setLocalSettings");
-  const cancelButton = document.getElementById("clearLocalSettings");
-  getSetingsStorage();
 
-    saveButton.addEventListener("click", ()=> {
-      saveEmailSettings();
-      saveProfileSettings();
-      timeZoneSetting();
-    });
-
-    cancelButton.addEventListener("click", ()=> {
-      clearSettings();
-    });
-    
-  }
-  
-};
 
 
 
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Our modules / classes
+import AppDashboard from './app.js';
+
+// Instantiate a new object using our modules/classes
+const myApp = new AppDashboard();
+
