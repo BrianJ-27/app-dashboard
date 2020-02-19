38be5857783552639cbbf527760aac76e0d7c93e
Brian Johnson <johnson8727@hotmail.com>
2020-02-19 18:05:27 -0500

    renamed js files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Our modules / classes
-import AppDashboard from './app.js';
+import AppDashboard from './dashboard.js';
 
 // Instantiate a new object using our modules/classes
 const myApp = new AppDashboard();
